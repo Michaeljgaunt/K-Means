5955b5c75d80b093d49aedbf56e8244590eb1e27
Michael Gaunt <michaeljgaunt@googlemail.com>
2015-06-20 02:30:11 +0100

    Version 0.93
    Code cleanup and added CL interface.

--- k_means.py
+++ k_means.py
@@ -1,4 +1,3 @@
-import random
 import numpy
 import collections
 from scipy.spatial import distance
@@ -33,10 +32,7 @@ def cluster_data(centers, data, c):
         #Iterating over each cluster center
         for j in xrange(0, centers_length):
             #Calculating euclidean distance of the vector from the cluster center.
-            if(centers_length == 5):
-                curr_dist = distance.euclidean(data[i][0:4:1], centers[j][0:4:1])
-            else:
-                curr_dist = distance.euclidean(data[i][0:4:1], centers[j][0:4:1])
+            curr_dist = distance.euclidean(data[i][0:4:1], centers[j][0:4:1])
             if(j == 0):
                 dist = curr_dist
                 cluster = 0
@@ -51,7 +47,7 @@ def cluster_data(centers, data, c):
             data[i][-1] = cluster
     return data
 
-#Function to caluclate the centers of the clusters.
+#Function to caluclate the mean centers of the clusters.
 def calculate_centers(data, k):
     data_length = len(data)
     new_centers = []
@@ -69,6 +65,29 @@ def calculate_centers(data, k):
             total[l] /= count   
         new_centers.append(total)
     return new_centers
+
+#Function to calculate the nearest data point to the mean cluster center (medoid)
+def calculate_medoids(data, centers, k):
+    medoids = []
+    data_length = len(data)
+    for i in xrange(0, k):
+        closest = 0
+        firstFlag = True
+        for j in xrange(0, data_length):
+            if(data[j][5] == i):
+                if(firstFlag):
+                    closest = j
+                    curr_dist = distance.euclidean(data[j][0:4:1], centers[i][0:4:1])
+                    firstFlag = False
+                else:
+                    if(distance.euclidean(data[j][0:4:1], centers[i][0:4:1]) < curr_dist):
+                        closest = j
+                        curr_dist = distance.euclidean(data[j][0:4:1], centers[i][0:4:1])
+        medoids.append(data[closest][0:4:1])
+    return medoids                    
+
+            
+            
 
 #Function to count the number of items in a cluster.
 def count_cluster_items(data, k):
@@ -132,7 +151,7 @@ def calc_dbi(k, d):
 
 #Function to perform DBI evaluation on a set of clusters.
 def evaluate_clusters_dbi(data, centers, count, k):
-    within_cluster_scatter = calc_within_cluster_scatter(clustered_data, centers, new_count, k)
+    within_cluster_scatter = calc_within_cluster_scatter(data, centers, count, k)
     sep1_2 = calc_between_cluster_spread(centers[0], centers[1])
     sep1_3 = calc_between_cluster_spread(centers[0], centers[2])
     sep2_3 = calc_between_cluster_spread(centers[1], centers[2])
@@ -141,50 +160,6 @@ def evaluate_clusters_dbi(data, centers, count, k):
     cm2_3 = calc_cluster_measure(within_cluster_scatter[1], within_cluster_scatter[2], sep2_3)
     dbi = calc_dbi(k, max(cm1_2, cm1_3, cm2_3))
     print "Davies-Bouldin Index for cluster set (lower is better):", dbi
-
-
-if __name__ == "__main__":
-    #Setting K value.
-    k = 3
-    #Generating vectors from the data file.
-    feat_vects = get_feat_vects("iris.data.txt", 5)
-    #Picking K random vectors to serve as initial cluster centres.
-    print "\nInitialising cluster centers..."
-    centers = random.sample(feat_vects, k)
-    #Performing initial clustering.
-    print "Clustering data..."
-    clustered_data = cluster_data(centers, feat_vects, True)
-    #Counting members in each cluster.
-    print "Counting cluster members..."
-    new_count = count_cluster_items(clustered_data, k)
-    print new_count
-    #Setting the flag and counter for the while loop.
-    converge_flag = False
-    counter = 1
-
-
-    #This block executes while the clusters have not converged
-    while not bool(converge_flag):
-        print "\nIteration ", counter
-        old_count = new_count
-        print "Calculating new cluster centers..."
-        centers = calculate_centers(clustered_data, k)
-        print "Clustering data..."
-        clustered_data = cluster_data(centers, feat_vects, False)
-        print "Counting cluster members..."
-        new_count = count_cluster_items(clustered_data, k)
-        print new_count
-        #Comparing the counts to check for convergence.
-        converge_flag = compare_counts(old_count, new_count, k)
-        counter += 1
-
-    print "\nCount has remained unchanged, clusters have converged to an optima."
-    print  "Evaluating clusters..."
-    #Evaluate clusters using simple metrics and also DBI.
-    evaluate_clusters_simple(clustered_data, new_count, k)
-    evaluate_clusters_dbi(clustered_data, centers, new_count, k)
-
-
 
 
 
---
+++ main.py
@@ -0,0 +1,71 @@
+import k_means
+import random
+import argparse
+import sys
+
+if __name__ == "__main__":
+    
+    #Adding an argparse to parse command line commands.
+    parser = argparse.ArgumentParser(description="K-Means clustering algorithm for Fisher's Iris Dataset.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    #Adding commands.
+    centmod = parser.add_mutually_exclusive_group(required=True)
+    centmod.add_argument("-c", "--centroids", help="Use Mean centroids as cluster centers", action='store_true')
+    centmod.add_argument("-m", "--medoids",help="Use Mean medoids as cluster centers", action='store_true')
+    
+    #Parsing the command line arguments.
+    args = parser.parse_args()
+
+    #Setting K value.
+    k = 3
+    #Generating vectors from the data file.
+    print "\nLoading dataset..."
+    feat_vects = k_means.get_feat_vects("iris.data.txt", 5)
+    #Picking K random vectors to serve as initial cluster centres.
+    print "Initialising cluster centers..."
+    centers = random.sample(feat_vects, k)
+    #Performing initial clustering.
+    print "Clustering data..."
+    clustered_data = k_means.cluster_data(centers, feat_vects, True)
+    #Counting members in each cluster.
+    print "Counting cluster members..."
+    new_count = k_means.count_cluster_items(clustered_data, k)
+    print new_count
+    #Setting the flag and counter for the while loop.
+    converge_flag = False
+    counter = 1
+
+
+    #This block executes while the clusters have not converged
+    while not bool(converge_flag):
+        print "\nIteration ", counter
+        old_count = new_count
+        if(args.centroids):
+            print "Calculating new cluster centers (mean centroids)..."
+            centers = k_means.calculate_centers(clustered_data, k)
+        elif(args.medoids):
+            print "Calculating new cluster centers (medoids)..."
+            centers = k_means.calculate_medoids(clustered_data, centers, k)
+        print "Clustering data..."
+        clustered_data = k_means.cluster_data(centers, feat_vects, False)
+        print "Counting cluster members..."
+        new_count = k_means.count_cluster_items(clustered_data, k)
+        print new_count
+        #Comparing the counts to check for convergence.
+        converge_flag = k_means.compare_counts(old_count, new_count, k)
+        counter += 1
+
+    print "\nCount has remained unchanged, clusters have converged to an optima."
+    print  "Evaluating clusters..."
+    #Evaluate clusters using simple metrics and also DBI.
+    k_means.evaluate_clusters_simple(clustered_data, new_count, k)
+    k_means.evaluate_clusters_dbi(clustered_data, centers, new_count, k)
+
+
+
+
+
+
+
+
+
+
